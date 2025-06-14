59bb6338ed0300fd30264dbcfb8634d625852608
delphizzz <130549779+delphizzz@users.noreply.github.com>
2025-06-14 21:07:55 +0700

    Update crack.js

--- crack.js
+++ crack.js
@@ -32,7 +32,7 @@ const bundle = {
 };
 
 const listua = {
-  'Lightune': { name: 'pro', id: 'Lightune_Pro_Year', cm: 'sja' } ,  //Lightune - AI 
+  'Lightune': { name: 'pro', id: 'Lightune_Pro_Year', cm: 'sjb' } ,  //Lightune - AI 
   'Ai': { name: 'annual', id: 'ai_video_year', cm: 'sjb' } ,   //AI Video 
   'Funswap': { name: 'annual', id: 'funswap_yearly', cm: 'sjb' }  //AI Video 
 };
